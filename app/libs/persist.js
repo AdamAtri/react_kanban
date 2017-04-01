094c5051bd3aa33fdda1bc16a9eba6a7c303e4f5
Adam Atri <k.adam.atri@gmail.com>
2017-04-01 18:08:57 -0400

    Adds a persistency module that bootstraps and snapshots application data.

---
+++ app/libs/persist.js
@@ -0,0 +1,15 @@
+// Persistency module for bootstrapping and snapshotting
+
+export default function(alt, storage, storageName) {
+  try {
+    alt.bootstrap(storage.get(storageName));
+  }
+  catch(e) {
+    console.error('Failed to bootstrap data.', e);
+  }
+  alt.FinalStore.listen( () => {
+    if (! storage.get('debug')) {
+      storage.set(storageName, alt.takeSnapshot());
+    }
+  });
+}
