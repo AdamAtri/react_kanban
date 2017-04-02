bac92b042f5ea0dcf67a1939d615668d759fdc66
Adam Atri <k.adam.atri@gmail.com>
2017-04-02 00:45:28 -0400

    Stubs data store and elements for Lanes.

--- app/components/Provider/setup.js
+++ app/components/Provider/setup.js
@@ -1,10 +1,12 @@
 import storage from '../../libs/storage';
 import persist from '../../libs/persist';
 import NoteStore from '../../stores/NoteStore';
+import LaneStore from '../../stores/LanesStore';
 
 // Exports a function that will connect the 'NoteStore' data store to `Alt`
 //  and persist 'alt' data to localStorage as 'app'.
 export default alt => {
   alt.addStore('NoteStore', NoteStore);
+  alt.addStore('LaneStore', LaneStore);
   persist(alt, storage(localStorage), 'app');
 }
