8e32871ae05be9e6c7ec2b2befb50096ab33200a
Developer <k.adam.atri@gmail.com>
2017-04-15 14:36:04 -0400

    Connects Lanes with App.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,79 +1,33 @@
 import React from 'react';
 import uuid from 'uuid';
-import Notes from './Notes';
 import AutoFocusTextInput from './AutoFocusTextInput';
 import connect from '../libs/connect';
-import NoteActions from '../actions/NoteActions';
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
 
-class App extends React.Component {
 
-  render() {
-    // as seen at the bottom of this file, the NoteStore
-    //  is injected into <props> through `connect`
-    const { notes } = this.props;
-    return (
-      <div>
-        <button
-          className="add-note"
-          onClick={ this.addNote }>
-          +
-        </button>
-        <Notes
-          notes={notes}
-          onNoteClick={ this.enableEditing }
-          onDelete={ this.deleteNote }
-          onEdit={ this.editNote } />
-      </div>
-    );
-  }
-
-  addLane = () => {
-    this.props.laneActions.create({name: 'New Lane'});
-  }
-
-  // Function: addNote
-  //  Connects to NoteActions and calls `create`
-  addNote = () => {
-    this.props.NoteActions.create({
+const App = ({LaneActions, lanes}) => {
+  const addLane = () => {
+    LaneActions.create({
       id: uuid.v4(),
-      task: 'New task'
+      name: 'New Lane'
     });
-  }
-  // Function: deleteNote
-  //  Connects to NoteActions to call `delete`
-  deleteNote = (id, e) => {
-    e.stopPropagation();
-    this.props.NoteActions.delete(id);
-  }
+  };
 
-  editNote = (id, task) => {
-    this.props.NoteActions.update({id, task, editing: false});
-  }
+  return (
+    <div>
+      <button className='add-lane' onClick={addLane}>+</button>
+      <Lanes lanes={lanes} />
+    </div>
+  );
+};
 
-  // Functon: _enableEditing
-  //  Creates a map function to enable editing on the specified id
-  //  and calls <_updateNotes>
-  enableEditing = (id) => {
-    this.props.NoteActions.update({id, editing: true});
-  }
 
-  // Function: _updateNotes
-  //  Updates state on notes.
-  //  Takes an `id` (optional) and a `func` that will
-  //  map onto the current <notes> array producing a new one.
-  _updateNotes = (id, func) => {
-    this.setState({
-      notes: this.state.notes.map(func)
-    });
-  }
-}
-
-// Use the Flux Connect to connect to the NotesStore data.
-//  Contents of the NotesStore will passed in props to the instance of App
-//  as `notes`. `NoteActions` provides basic CRUD operations on `notes`.
-//  NoteActions will be available as `this.props.NoteActions`
-export default connect(({notes}) => ({
-  notes
+// Use the Flux Connect to connect to the LaneStore data.
+//  Contents of the LaneStore will passed in props to the instance of App
+//  as `lanes`. `LaneActions` provides basic CRUD operations on `lanes`.
+export default connect(({lanes}) => ({
+  lanes
 }), {
-  NoteActions
+  LaneActions
 })(App)
--- app/components/Provider/setup.js
+++ app/components/Provider/setup.js
@@ -1,10 +1,14 @@
 import storage from '../../libs/storage';
 import persist from '../../libs/persist';
 import NoteStore from '../../stores/NoteStore';
+import LaneStore from '../../stores/LaneStore';
 
-// Exports a function that will connect the 'NoteStore' data store to `Alt`
-//  and persist 'alt' data to localStorage as 'app'.
+
+// Exports a function that will connect the 'NoteStore' and 'LaneStore'
+//  data store to `Alt` and persist 'alt' data to localStorage as 'app'.
 export default alt => {
   alt.addStore('NoteStore', NoteStore);
+  alt.addStore('LaneStore', LaneStore);
+  // persists <alt> data to localStorage as 'app'
   persist(alt, storage(localStorage), 'app');
 }
