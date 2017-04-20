0821799aadd3de1e2252ff42b46ad518b5892639
Developer <k.adam.atri@gmail.com>
2017-04-19 23:57:13 -0400

    Sets Notes up as DropTarget that can detect another Note hovering over.'

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 // Import Drag-n-Drop packs
-import {DragSource} from 'react-dnd';
+import {DragSource, DropTarget} from 'react-dnd';
+import { compose } from 'redux';
 import ItemTypes from '../constants/itemTypes';
 
 // Define the <Note> element
-const Note = ({connectDragSource, children, ...props}) => {
-  return connectDragSource(
+const Note = ({connectDragSource, connectDropTarget, children, ...props}) => {
+  return compose(connectDragSource, connectDropTarget)(
     <div {...props}>
       {children}
     </div>
   );
-}
-// Spec: noteSource
+};
+// Spec: noteSource (DragSource)
 //  NoteSource is a JS object that describes how the
 //  the drag source reacts to the drag and drop events.
 //  Minimally, must contain a function `beginDrag`, but
@@ -22,9 +23,25 @@ const noteSource = {
     console.log('begin dragging note', props, monitor, component);
     return {};
   }
+};
+// Spec: noteTarget (DropTarget)
+//  NoteTarget is a JS object that descibes how the
+//  note will respond to having another note dropped onto it.
+//  *all functions here are optional (can also support `drop`, `canDrop`)
+//  see: https://react-dnd.github.io/react-dnd/docs-drop-target.html
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+    console.log('dragging note is over me', sourceProps, targetProps);
+  }
 }
 
 // export the note in as a draggable item
-export default DragSource(ItemTypes.NOTE, noteSource, connect => ({
-  connectDragSource: connect.dragSource()
-}))(Note);
+export default compose(
+  DragSource(ItemTypes.NOTE, noteSource, connect => ({
+    connectDragSource: connect.dragSource()
+  })),
+  DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
+    connectDropTarget: connect.dropTarget()
+  }))
+)(Note);
